c1fcc7cd0f317419e516af846a5c8c5ee032e034
zhangchangyue123-netizen <zhangchangyue123@gmail.com>
2026-07-28 15:34:39 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+const polaroid = document.querySelector(".polaroid");
 
+let scale = 1;
+let direction = 1;
+
+function breathe(){
+
+    scale += direction * 0.00025;
+
+    if(scale > 1.02){
+
+        direction = -1;
+
+    }
+
+    if(scale < 1){
+
+        direction = 1;
+
+    }
+
+    polaroid.style.transform =
+    `rotate(-3deg) scale(${scale})`;
+
+    requestAnimationFrame(breathe);
+
+}
+
+breathe();
